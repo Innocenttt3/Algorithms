95f685b2ea8471cc46709a710f446232f3208bab
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2023-12-04 16:32:59 +0100

    powrot na enterprise done

--- statki/main.cpp
+++ statki/main.cpp
@@ -1,53 +1,99 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
-using namespace std;
+struct Connection {
+    int travelTime;
+    int destinationShipId;
 
-struct Ship {
-    vector<pair<int, int>> connections;
+    Connection(int shipID, int travelTime) : travelTime(travelTime), destinationShipId(shipID) {}
 };
 
-int main() {
-    int dataSets;
-    cin >> dataSets;
-    while (dataSets--) {
-        int shipsAmount, teleportsAvailable, spockLocation;
-        cin >> shipsAmount >> teleportsAvailable >> spockLocation;
-        vector<Ship> ships(shipsAmount);
-        for (int i = 0; i < teleportsAvailable; i++) {
-            int ship1, ship2, travelTime;
-            cin >> ship1 >> ship2 >> travelTime;
-            ships[ship1].connections.push_back({ship2, travelTime});
-            ships[ship2].connections.push_back({ship1, travelTime});
-        }
-        vector<int> blockedUntil(shipsAmount, 0);
-        for (int i = 0; i < shipsAmount; i++) {
-            int amount;
-            cin >> amount;
-            for (int j = 0; j < amount; j++) {
-                int time;
-                cin >> time;
-                blockedUntil[i] = max(blockedUntil[i], time);
+struct Ship {
+    bool visited;
+    int id;
+    std::vector<Connection> connectedShips;
+    std::vector<int> secondsToStop;
+};
+
+void Dijkstra(std::vector<Ship>& ships,int startShipId, int endShipId) {
+    std::vector distances(ships.size(), 2147483647);
+
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> queue;
+    queue.push({0, startShipId});
+    distances[startShipId] = 0;
+
+    while (!queue.empty()) {
+        int currentShipId = queue.top().second;
+        queue.pop();
+
+        if (ships[currentShipId].visited) continue;
+        ships[currentShipId].visited = true;
+
+        if (currentShipId == endShipId) break;
+
+        int tmpTravel = 0;
+
+        for (int tmpShip : ships[0].secondsToStop) {
+            if(tmpShip == 1) {
+                tmpTravel++;
             }
         }
-        vector<int> dist(shipsAmount, 2e9);
-        dist[spockLocation] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, spockLocation});
-        while (!pq.empty()) {
-            int d = pq.top().first, u = pq.top().second;
-            pq.pop();
-            if (d != dist[u]) continue;
-            for (auto &v : ships[u].connections) {
-                if (dist[u] + v.second + blockedUntil[v.first] < dist[v.first]) {
-                    dist[v.first] = dist[u] + v.second + blockedUntil[v.first];
-                    pq.push({dist[v.first], v.first});
+        for (Connection& connection : ships[currentShipId].connectedShips) {
+            int destinationShipId = connection.destinationShipId;
+            int travelTime = connection.travelTime;
+            int newDistance = distances[currentShipId] + travelTime;
+
+            for (int stopTime : ships[destinationShipId].secondsToStop) {
+                if (newDistance == stopTime) {
+                    newDistance++;
                 }
             }
+
+            if (newDistance < distances[destinationShipId]) {
+                distances[destinationShipId] = newDistance;
+                queue.push({newDistance, destinationShipId});
+            }
         }
-        cout << dist[0] << '\n';
     }
+
+    std::cout << distances[endShipId] << std::endl;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int dataSets;
+    int shipsAmount;
+    int teleportsAvailable;
+    int spockLocation;
+    int amount;
+
+    int tmpShip1id, tmpShip2id;
+    int tmpTravelTime;
+
+    std::cin >> dataSets;
+    for (int y = 0; y < dataSets; y++) {
+        std::cin >> shipsAmount >> teleportsAvailable >> spockLocation;
+        std::vector<Ship> ships(shipsAmount);
+        for (int i = 0; i < teleportsAvailable; i++) {
+            std::cin >> tmpShip1id >> tmpShip2id >> tmpTravelTime;
+            ships[tmpShip1id].connectedShips.push_back(Connection(tmpShip2id, tmpTravelTime));
+            ships[tmpShip2id].connectedShips.push_back(Connection(tmpShip1id, tmpTravelTime));
+            ships[tmpShip1id].id = tmpShip1id;
+        }
+        for (int j = 0; j < shipsAmount; j++) {
+            std::cin >> amount;
+            for (int x = 0; x < amount; x++) {
+                int num;
+                std::cin >> num;
+                ships[j].secondsToStop.push_back(num);
+            }
+        }
+        Dijkstra(ships, spockLocation, 0);
+    }
+
     return 0;
 }
