4a50ecade11c917f5e61c313ce90df93b7cd8b91
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-01-28 22:46:33 +0100

    kopiec first try

--- podroz/main.cpp
+++ podroz/main.cpp
@@ -3,70 +3,57 @@
 
 struct City {
     std::string nameOfCity;
-    int valueNeeded;
+    long int valueNeeded;
 
-    City() {
-    };
+    City() {}
 };
 
-void addToTree(std::string name, int value, int index, City arr[]) {
-    int tmpValue;
-    int parentIndex = 0;
-    std::string tmpName;
 
-    if (index % 2 == 0) {
-        parentIndex = (index - 2) / 2;
-    }
-    else if (index % 2 == 1) {
-        parentIndex = (index - 1) / 2;
-    }
+void addToTree(std::string name, long int value, long int index, City arrOfCities[]) {
+    arrOfCities[index].nameOfCity = name;
+    arrOfCities[index].valueNeeded = value;
 
-    if (index > 0) {
-        if (value > arr[parentIndex].valueNeeded) {
-            tmpValue = value;
-            value = arr[parentIndex].valueNeeded;
-            arr[index].valueNeeded = tmpValue;
-            tmpName = name;
-            name = arr[parentIndex].nameOfCity;
-            arr[index].nameOfCity = tmpName;
+    while (index > 0) {
+        int parentIndex = (index - 1) / 2;
+        if (arrOfCities[parentIndex].valueNeeded < arrOfCities[index].valueNeeded) {
+            City temp = arrOfCities[parentIndex];
+            arrOfCities[parentIndex] = arrOfCities[index];
+            arrOfCities[index] = temp;
+            index = parentIndex;
+        } else {
+            break;
         }
-        else if (value == arr[parentIndex].valueNeeded && name > arr[parentIndex].nameOfCity) {
-            tmpValue = value;
-            value = arr[parentIndex].valueNeeded;
-            arr[index].valueNeeded = tmpValue;
-            tmpName = name;
-            name = arr[parentIndex].nameOfCity;
-            arr[index].nameOfCity = tmpName;
-        }
-        else {
-            arr[index].nameOfCity = name;
-            arr[index].valueNeeded = value;
-        }
-        addToTree(arr[parentIndex].nameOfCity, arr[parentIndex].valueNeeded, parentIndex, arr);
-    }
-    else if (index == 0) {
-        arr[index].nameOfCity = name;
-        arr[index].valueNeeded = value;
-    }
-    else {
-        std::cout << "out of index" << std::endl;
     }
 }
 
 int main() {
+
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int amountOfData;
-    int tmpValue;
-    std::string tmpName;
+    long int amountOfData;
+    long int maxValue;
+
     std::cin >> amountOfData;
-    City arr *[amountOfData];
-    for (int i = 0; i < amountOfData; i++) {
+    City* arrOfCities = new City[amountOfData];
+
+    for (long int i = 0; i < amountOfData; ++i) {
+        std::string tmpName;
+        long int tmpValue;
         std::cin >> tmpName >> tmpValue;
-        addToTree(tmpName, tmpValue, i, *arr);
+        addToTree(tmpName, tmpValue, i, arrOfCities);
     }
 
+    std::cin >> maxValue;
+
+    for (long int i = 0; i < amountOfData; ++i) {
+        if (maxValue > arrOfCities[i].valueNeeded) {
+            maxValue -= arrOfCities[i].valueNeeded;
+            std::cout << arrOfCities[i].nameOfCity << " ";
+        }
+    }
+
+    delete[] arrOfCities;
     return 0;
 }
